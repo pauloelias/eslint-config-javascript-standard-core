4af442c56014bd1436c30857cd7b0a229366b381
Paulo Elias <paulo@pauloelias.com>
2020-09-22 15:28:23 -0700

    Remove Prettier and define some base rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,24 @@
 module.exports = {
-  extends: ['standard'],
+  plugins: ['html', 'import', 'node', 'promise', 'standard'],
+  extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:node/recommended', 'plugin:promise/recommended', 'standard'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true
+    }
+  },
+  rules: {
+    'no-debugger': 1,
+    'no-alert': 1,
+    quotes: [2, 'single', 'avoid-escape']
   },
   env: {
     browser: true,
-    commonjs: true,
-    node: true,
-    jest: true
-  },
-  plugins: ['html', 'standard']
+    es2020: true,
+    jest: true,
+    jquery: true,
+    node: true
+  }
 }
